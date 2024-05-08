bd20b5a4467540a533e74f1822992f0f960dec7d
OksanaKukushkina <oksundel@mail.ru>
2024-05-08 22:14:19 +0200

    Add mapbox token

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -1,28 +1,42 @@
-import * as Leaflet from 'leaflet';
-import { useEffect, useState } from 'react';
-import './styles.css';
-import Canvas from '../canvas/Canvas';
+import * as Leaflet from "leaflet";
+import { useEffect, useState } from "react";
+import "./styles.css";
+import Canvas from "../canvas/Canvas";
+import { MAPBOX_TOKEN } from "../../constants";
 
 export const LEAFLET_OPTIONS = {
     zoomControl: true,
     loadingControl: true,
     attributionControl: false,
+    accessToken: MAPBOX_TOKEN,
 };
 
 export default function Map() {
     const [map, setMap] = useState<Leaflet.Map | null>(null);
 
     useEffect(() => {
-        setMap(
-            Leaflet.map('mapid', LEAFLET_OPTIONS).setView(
+        setMap(() => {
+            const leafletMap = Leaflet.map("mapid", LEAFLET_OPTIONS).setView(
                 [55.6739075, 12.5692004],
-                20
-            )
-        );
+                17
+            );
+
+            Leaflet.tileLayer(
+                "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+                {
+                    tileSize: 512,
+                    zoomOffset: -1,
+                    id: "mapbox/satellite-v9",
+                    accessToken: MAPBOX_TOKEN,
+                }
+            ).addTo(leafletMap);
+
+            return leafletMap;
+        });
     }, []);
 
     return (
-        <div id='mapid' className='map'>
+        <div id="mapid" className="map">
             {map && <Canvas map={map} />}
         </div>
     );
---
+++ src/constants.ts
@@ -0,0 +1,7 @@
+export const MAPBOX_TOKEN =
+  "pk.eyJ1Ijoib2tzYW5ha3VrdXNoa2luYSIsImEiOiJjbHZ4cHhidzAyNzIyMmttc20wMTN5OHppIn0.zB94td__q-dYREcaeUJpwA";
+
+export enum TeamName {
+  Red = "red",
+  Blue = "blue",
+}
